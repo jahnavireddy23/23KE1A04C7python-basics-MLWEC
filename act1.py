dd5165626c38dbe6dc2a41a2c995a55b558ba305
jahnavireddy23 <vuyyuru.jahnavi04@gmail.com>
2025-07-30 13:49:18 +0530

    adding all files

---
+++ act1.py
@@ -0,0 +1,8 @@
+#n=int(input("Enter a number: "))
+#print("Even" if n % 2 ==0 else "Odd")
+number=int(input("Enter a number: "))
+if number %2==0:
+    print("even number")
+else:
+    print("odd number")
+
