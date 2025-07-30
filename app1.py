990a3dd92a51e1d86c316e48bcc00bc3d1061bcf
jahnavireddy23 <vuyyuru.jahnavi04@gmail.com>
2025-07-30 13:45:13 +0530

    uploading app1.py

---
+++ app1.py
@@ -0,0 +1,12 @@
+name="Jahnavi Reddy"
+number=10.0
+isOn=True
+letter="J"
+#arithematic - + - * / %
+#logical - AND,OR,NOT
+#comparison - <,>,==,!=,<=,>=
+#assignment - =,+=,-=,-+,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)
